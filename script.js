2500831c22256746ad9ee1a5541d213fc542f464
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-28 19:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function () {
 	var bandName = ["The Beatles", "Anthrax", "Led Zeppelin", "Aerosmith", "The Rolling Stones"];
 	function removeArticles(bandName){
 		const lowerCaseNames = bandName.toLowerCase();
-		if(lowerCaseNames.startsWith('a')||lowerCaseNames.startsWith('an')||lowerCaseNames.startsWith('the')){
+		if(lowerCaseNames.startsWith('a ')||lowerCaseNames.startsWith('an ')||lowerCaseNames.startsWith('the ')){
 			return bandName.slice(lowerCaseNames.indexOf(' ')+1);		
 		}
 	    return bandName; 
@@ -16,7 +16,7 @@ window.onload = function () {
 	
 	const ulElements = document.getElementById("band");
 	
-	for(let i = 0; i < bandNamesWithoutArticles; i++){
+	for(let i = 0; i < bandNamesWithoutArticles.length; i++){
 	    const bandList = bandNamesWithoutArticles[i];
 	    const liElement = document.createElement("li")
 	    liElement.textContent = bandList 
